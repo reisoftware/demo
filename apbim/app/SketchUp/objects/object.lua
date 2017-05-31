9650903dff89919c74df29252186d6dd59d37499
SJY2016 <1433653386@qq.com>
2017-05-31 09:03:51 +0800

    change draw shape !

--- apbim/app/SketchUp/objects/object.lua
+++ apbim/app/SketchUp/objects/object.lua
@@ -92,17 +92,33 @@ function Class:init_surfaces()
 	return surfaces
 end
 
-function Class:on_draw_rendering()
-	if type(self.points) ~= 'table' then return end 
-	local obj = {}
-	obj.surfaces = self:init_surfaces() or {}
-	self:set_shape_rendering(obj)
+-- function Class:on_draw_rendering()
+	-- if type(self.points) ~= 'table' then return end 
+	-- local obj = {}
+	-- obj.surfaces = self:init_surfaces() or {}
+	-- self:set_shape_rendering(obj)
 	
+-- end
+
+-- function Class:on_draw_diagram()
+	-- if type(self.points) ~= 'table' then return end 
+	-- local obj = {}
+	-- obj.surfaces = self:init_lines_surfaces() or {}
+	-- self:set_shape_diagram(obj)
+-- end
+
+function Class:on_draw(arg)
+	if type(self.points) ~= 'table' then return end 	
+	local obj = {}
+	local fs = {};
+	fs.Wireframe = function()
+		obj.surfaces = self:init_lines_surfaces()
+		return obj
+	end
+	fs.Rendering = function()
+		obj.surfaces =  self:init_surfaces() or {}
+		return obj
+	end
+	if type(fs[arg.mode])=='function' then return fs[arg.mode]() end
 end
 
-function Class:on_draw_diagram()
-	if type(self.points) ~= 'table' then return end 
-	local obj = {}
-	obj.surfaces = self:init_lines_surfaces() or {}
-	self:set_shape_diagram(obj)
-end
